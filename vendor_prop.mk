f93471f2943ada7785a1b9040e72eb87820ccb9d
Gabor <meszaros.gabor.10k@gmail.com>
2019-01-03 09:12:41 +0100

    prop: DPM config change
    Enable  TCP Connection Manager (TCM) and Fast Dormancy (FD)

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -65,7 +65,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # CNE & DPM
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1 \
-    persist.vendor.dpm.feature=11 \
+    persist.vendor.dpm.feature=9 \
     persist.env.fastdorm.enabled=true
 
 # Data modules
