f5de176fea2d448d44531fed533c20861361a4ac
mesziman <meszaros.gabor.10k@gmail.com>
2019-04-10 09:32:41 +0000

    boardconfig:compile kernel 9.0.2 clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,8 +52,10 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8996
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 9.0.2
+#KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu/bin
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8996
