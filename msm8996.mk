4e91867dfc4288130a27dabb5d73d9c00cc85c40
HENDY <hendyaugustama@gmail.com>
2019-06-26 15:14:09 +0700

    msm8996-common: Offline charging[1]

--- msm8996.mk
+++ msm8996.mk
@@ -325,6 +325,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
     $(LOCAL_PATH)/configs/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml
 
+# Charger
+include device/xiaomi/capricorn/rootdir/charger/charger.mk
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service.gemini-libperfmgr
